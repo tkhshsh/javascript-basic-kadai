a5de6d0071aa0f2ea6770672fc2ab5dfb336e8fb
tkhshsh <xlxolxus@gmail.com>
2024-03-31 07:14:24 +0900

    kadai_020を修正

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,12 @@
+//htmlからbtnというidを持つHTML要素を取得し、定数btnに代入する
 const btn = document.getElementById('btn');
+
+//htmlからtextというidを持つHTML要素を取得し、定数textに代入する
 const text = document.getElementById('text')
 
+//HTML要素であるbtnがclickされた時に
 btn.addEventListener('click',() => {
-   text.textContent = 'ボタンをクリックしました'
-    // console.log('ボタンをクリックしました')
+ //text（event.htmlのh２）にボタンをクリックしましたというテキストを追加する）
+    text.textContent = 'ボタンをクリックしました'
 });
 

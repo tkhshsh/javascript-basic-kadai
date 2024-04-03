a39e0ac2dd12419ab50f86e0803c96b7050875f6
tkhshsh <xlxolxus@gmail.com>
2024-04-03 09:07:27 +0900

    kadai_021を修正

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -4,9 +4,27 @@ const btn = document.getElementById('btn');
 //htmlからtextというidを持つHTML要素を取得し、定数textに代入する
 const text = document.getElementById('text')
 
-//非同期処理（setTimeout関数）
-setTimeout(() => {
-  //処理(htmlのtextにボタンをクリックしましたと表示する)
-  text.textContent = 'ボタンをクリックしました';
+
+
+
+//HTML要素であるbtnがclickされた時に
+btn.addEventListener('click',() => {
+  //text（event.htmlのh２）にボタンをクリックしましたというテキストを追加する）
+  if(btn.click){
+    //非同期処理を行うためsetTime関数を使用
+    setTimeout(( ) => {
+      //HTMLのtextに'ボタンをクリックしました'と追加する
+      text.textContent = 'ボタンをクリックしました'
+    //２秒後に
+    },2000)};
+ });
+
+ /*
+ if(btn.click){
+//非同期処理を行うためsetTime関数を使用
+setTimeout(( ) => {
+  //HTMLのtextに'ボタンをクリックしました'と追加する
+  text.textContent = 'ボタンをクリックしました'
 //２秒後に
-},2000);
+},2000)};
+*/
